9f382e41b6bbe1c22542cdffebc1eb0aaada8c17
nasim0994 <nasimuddin0994@gmail.com>
2022-12-30 12:40:13 +0600

    change

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Aboutus from "../../components/Aboutus/Aboutus";
 import Banner from "../../components/Banner/Banner";
+import CompanyInfo from "../../components/CompanyInfo/CompanyInfo";
+import Services from "../../components/Services/Services";
 
 const Home = () => {
   return (
     <section className="w-[90%] mx-auto">
       <Banner />
+      <CompanyInfo />
+      <Services />
+      <Aboutus />
     </section>
   );
 };
